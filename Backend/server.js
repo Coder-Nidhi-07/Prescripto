00edce11b0c693996d09e186ddde90e9a13539af
Coder-Nidhi-07 <nidhijha350@gmail.com>
2025-06-09 11:14:03 +0530

    Revert "admin authentication"
    This reverts commit 81e31e848851bbf007a720cead1d4964629fb7d1.

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,10 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
-import adminRouter from './routes/adminRoute.js'
-
 
 // Appm config
 const app=express()
 const port=process.env.PORT || 4000
-connectDB()
-connectCloudinary()
 
 // middleware
 app.use(express.json())
@@ -18,9 +12,6 @@ app.use(cors())
 
 
 // apoi endpoint
-app.use('/api/admin',adminRouter)
-// localhost:4000/api/admin/add-doctor
-
 app.get('/',(req,res)=>{
     res.send('Api is working again with nodemon i dont know how its updating realtime')
 })
